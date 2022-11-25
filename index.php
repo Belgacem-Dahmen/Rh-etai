8e2162bf1282833e719a39386b1f87e04030cf84
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2022-11-25 17:00:08 +0100

    commit 25/11

---
+++ index.php
@@ -0,0 +1,262 @@
+
+<?php include 'includes/Header.php';?>
+<body>
+
+  
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1" data-aos="fade-up">
+          <div>
+            <h1>RH-ETAI </h1>
+            <h2>RH-ETAI est une WebApp concue pour le service Ressource Humaine de la société ETAI , filiére du groupe Infopro Digital en Tunisie.</h2>
+            <a href="#" class="download-btn"><i class="bx bxl-play-store"></i> Connexion </a>
+            <a href="#" class="download-btn"><i class="bx bxl-apple"></i> Nous Contacter</a>
+          </div>
+        </div>
+        <div class="col-lg-6 d-lg-flex flex-lg-column align-items-stretch order-1 order-lg-2 hero-img" data-aos="fade-up">
+          <img src="assets/img/hero-img.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= App Features Section ======= -->
+    <section id="features" class="features">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Notre Application RH-ETAI</h2>
+          <p>RH-ETAI , crée en 2022 spécialement pour le Service RH de la socité ETAI , dans le cadre d'un projet de fin de formation par Belgacem Dahmen, 
+            Nous vous presentons dans cette rubrique quelques details des services offerts dans cette application : RH-ETAI facilitera la gestion des ressources , employés , Fournisseurs , Contacts et tants d'autres... </p>
+        </div>
+
+        <div class="row no-gutters">
+          <div class="col-xl-7 d-flex align-items-stretch order-2 order-lg-1">
+            <div class="content d-flex flex-column justify-content-center">
+              <div class="row">
+                <div class="col-md-6 icon-box" data-aos="fade-up">
+                  <i class="bx bx-receipt"></i>
+                  <h4>Gestion des Employés </h4>
+                  <p>Créer un employé ,le modifier , le mettre à jour , garder un historique depuis sa creation. </p>
+                </div>
+                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+                  <i class="bx bx-cube-alt"></i>
+                  <h4>Gestion des recrutments</h4>
+                  <p>Rationalisez le planning interne et la coordination avec les responsables recrutement. Suivez facilement les candidats engagés dans le processus à l’aide d’un suivi à chaque étape.t</p>
+                </div>
+                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+                  <i class="bx bx-images"></i>
+                  <h4>Processus d’intégration</h4>
+                  <p>Suivez facilement vos nouveaux integrants avec ce modèle intuitif.</p>
+                </div>
+                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
+                  <i class="bx bx-shield"></i>
+                  <h4>Suivi des vacances</h4>
+                  <p>Rendez le suivi des jours de congé beaucoup moins pénible avec un modèle totalement personnalisable et facile à utiliser.</p>
+                </div>
+                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
+                  <i class="bx bx-atom"></i>
+                  <h4>Motivation des employés</h4>
+                  <p>Mesurez facilement l'engagement de vos employés et partagez avec eux les événements associés.</p>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+          <div class="image col-xl-5 d-flex align-items-stretch justify-content-center order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <img src="assets/img/features.svg" class="img-fluid" alt="">
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End App Features Section -->
+
+    <!-- ======= Details Section ======= -->
+    <section id="details" class="details">
+      <div class="container">
+
+        <div class="row content">
+          <div class="col-md-4" data-aos="fade-right">
+            <img src="assets/img/details-1.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-md-8 pt-4" data-aos="fade-up">
+            <h3> le meilleur logiciel RH pour les entreprises de toutes tailles - axé sur l'engagement collaborateur</h3>
+            <p class="fst-italic">
+            RH-ETAI est un outil de gestion de projet conçu pour vous aider à organiser et superviser le travail de vos équipes.
+            </p>
+            <ul>
+              <li><i class="bi bi-check"></i> Le recrutement.</li>
+              <li><i class="bi bi-check"></i> Le suivi de la performance.</li>
+              <li><i class="bi bi-check"></i> L'engagement des collaborateurs.</li>
+              <li><i class="bi bi-check"></i> Le suivi des feedbacks, etc.</li>
+            </ul>
+            <p>
+              Voluptas nisi in quia excepturi nihil voluptas nam et ut. Expedita omnis eum consequatur non. Sed in asperiores aut repellendus. Error quisquam ab maiores. Quibusdam sit in officia
+            </p>
+          </div>
+        </div>
+
+        <div class="row content">
+          <div class="col-md-4 order-1 order-md-2" data-aos="fade-left">
+            <img src="assets/img/details-2.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
+            <h3>Les avantages de RH-ETAI</h3>
+            </p>
+            <ul>
+              <li><i class="bi bi-check"></i> Un vrai gain de temps dans la réalisation, l'analyse et le suivi des reportings RH.</li>
+              <li><i class="bi bi-check"></i> Une vue complète, compréhensible et synthétique de toutes les données RH</li>
+              <li><i class="bi bi-check"></i> Une plateforme intuitive </li>
+              <li><i class="bi bi-check"></i> Une intégration avec de nombreuses solutions </li>
+            </ul>
+            <p>
+          </div>
+        </div>
+
+        
+
+      </div>
+    </section><!-- End Details Section -->
+
+ 
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Témoignages</h2>
+          <p>Vous trouverez ci dessous des commentaires sur des utilisateurs nomades qui ont téstés notre application pendant la periode de son pilotage.</p>
+        </div>
+
+        <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+          <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
+                <h3>Ayari Hamza</h3>
+                <h4>Agent commercial</h4>
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Application Simple et intuitve, ca me facilite la vie !
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div>
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
+                <h3>Rachida Khili</h3>
+                <h4>Gestionnaire d'apps</h4>
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Un vrai bijou, assez simple, gestion des equipes et des plannings sans trop de complexité , j'adore..
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div>
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
+                <h3>Jbeli Mehdi  </h3>
+                <h4>DSI</h4>
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  Application assez simple et intuitive , permet la gestion d'une petite et moyenne entreprises , il faut s'abonner sans hesitation.
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+              </div>
+            </div>
+
+            
+
+            
+
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+
+
+    
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6">
+            <div class="row">
+              <div class="col-lg-6 info">
+                <i class="bx bx-map"></i>
+                <h4>Address</h4>
+                <p>A108 Adam Street,<br>New York, NY 535022</p>
+              </div>
+              <div class="col-lg-6 info">
+                <i class="bx bx-phone"></i>
+                <h4>Call Us</h4>
+                <p>+1 5589 55488 55<br>+1 5589 22548 64</p>
+              </div>
+              <div class="col-lg-6 info">
+                <i class="bx bx-envelope"></i>
+                <h4>Email Us</h4>
+                <p>contact@example.com<br>info@example.com</p>
+              </div>
+              <div class="col-lg-6 info">
+                <i class="bx bx-time-five"></i>
+                <h4>Working Hours</h4>
+                <p>Mon - Fri: 9AM to 5PM<br>Sunday: 9AM to 1PM</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form" data-aos="fade-up">
+              <div class="form-group">
+                <input placeholder="Your Name" type="text" name="name" class="form-control" id="name" required>
+              </div>
+              <div class="form-group mt-3">
+                <input placeholder="Your Email" type="email" class="form-control" name="email" id="email" required>
+              </div>
+              <div class="form-group mt-3">
+                <input placeholder="Subject" type="text" class="form-control" name="subject" id="subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea placeholder="Message" class="form-control" name="message" rows="5" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <?php include 'includes/Footer.php';?>
